08fe8f75a5751db811a0f5e7a191e1b74c4226ea
Antz <billy1arm@users.noreply.github.com>
2020-12-21 17:09:13 +0000

    Fix some errors and warnings

--- Setup/realmdLoadDB.sql
+++ Setup/realmdLoadDB.sql
@@ -22,13 +22,13 @@ DROP TABLE IF EXISTS `db_version`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `db_version` (
-  `version` int(3) NOT NULL COMMENT 'The Version of the Release',
-  `structure` int(3) NOT NULL COMMENT 'The current core structure level.',
-  `content` int(3) NOT NULL COMMENT 'The current core content level.',
-  `description` varchar(30) NOT NULL DEFAULT '' COMMENT 'A short description of the latest database revision.',
-  `comment` varchar(150) DEFAULT '' COMMENT 'A comment about the latest database revision.',
+  `version` INT(3) NOT NULL COMMENT 'The Version of the Release',
+  `structure` INT(3) NOT NULL COMMENT 'The current core structure level.',
+  `content` INT(3) NOT NULL COMMENT 'The current core content level.',
+  `description` VARCHAR(30) NOT NULL DEFAULT '' COMMENT 'A short description of the latest database revision.',
+  `comment` VARCHAR(150) DEFAULT '' COMMENT 'A comment about the latest database revision.',
   PRIMARY KEY (`version`,`structure`,`content`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='Used DB version notes';
+) ENGINE=INNODB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,29 +50,29 @@ DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'The unique account ID.',
-  `username` varchar(32) NOT NULL DEFAULT '' COMMENT 'The account user name.',
-  `sha_pass_hash` varchar(40) NOT NULL DEFAULT '' COMMENT 'This field contains the encrypted SHA1 password.',
-  `gmlevel` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'The account security level.',
-  `sessionkey` longtext DEFAULT NULL COMMENT 'The Session Key.',
-  `v` longtext DEFAULT NULL COMMENT 'The validated Hash Value.',
-  `s` longtext DEFAULT NULL COMMENT 'Password ''Salt'' Value.',
-  `email` text DEFAULT NULL COMMENT 'The e-mail address associated with this account.',
-  `joindate` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'The date when the account was created.',
-  `last_ip` varchar(30) NOT NULL DEFAULT '0.0.0.0' COMMENT 'The last IP used by the person who last logged into the account.',
-  `failed_logins` int(11) unsigned NOT NULL DEFAULT 0 COMMENT 'The number of failed logins attempted on the account.',
-  `locked` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'Indicates whether the account has been locked or not.',
-  `last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'The date when the account was last logged into.',
-  `active_realm_id` int(11) unsigned NOT NULL DEFAULT 0 COMMENT 'Which maximum expansion content a user has access to.',
-  `expansion` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'Which maximum expansion content a user has access to.',
-  `mutetime` bigint(40) unsigned NOT NULL DEFAULT 0 COMMENT 'The time, in Unix time, when the account will be unmuted.',
-  `locale` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'The locale used by the client logged into this account.',
-  `os` varchar(3) DEFAULT '' COMMENT 'The Operating System of the connected client',
-  `playerBot` bit(1) NOT NULL DEFAULT b'0' COMMENT 'Determines whether the account is a User or a PlayerBot',
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'The unique account ID.',
+  `username` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'The account user name.',
+  `sha_pass_hash` VARCHAR(40) NOT NULL DEFAULT '' COMMENT 'This field contains the encrypted SHA1 password.',
+  `gmlevel` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'The account security level.',
+  `sessionkey` LONGTEXT DEFAULT NULL COMMENT 'The Session Key.',
+  `v` LONGTEXT DEFAULT NULL COMMENT 'The validated Hash Value.',
+  `s` LONGTEXT DEFAULT NULL COMMENT 'Password ''Salt'' Value.',
+  `email` TEXT DEFAULT NULL COMMENT 'The e-mail address associated with this account.',
+  `joindate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT 'The date when the account was created.',
+  `last_ip` VARCHAR(30) NOT NULL DEFAULT '0.0.0.0' COMMENT 'The last IP used by the person who last logged into the account.',
+  `failed_logins` INT(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'The number of failed logins attempted on the account.',
+  `locked` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Indicates whether the account has been locked or not.',
+  `last_login` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT 'The date when the account was last logged into.',
+  `active_realm_id` INT(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Which maximum expansion content a user has access to.',
+  `expansion` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Which maximum expansion content a user has access to.',
+  `mutetime` BIGINT(40) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'The time, in Unix time, when the account will be unmuted.',
+  `locale` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'The locale used by the client logged into this account.',
+  `os` VARCHAR(3) DEFAULT '' COMMENT 'The Operating System of the connected client',
+  `playerBot` BIT(1) NOT NULL DEFAULT b'0' COMMENT 'Determines whether the account is a User or a PlayerBot',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_username` (`username`),
   KEY `idx_gmlevel` (`gmlevel`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Account System';
+) ENGINE=INNODB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Account System';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -82,10 +82,10 @@ CREATE TABLE `account` (
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
 INSERT INTO `account` (`id`, `username`, `sha_pass_hash`, `gmlevel`, `sessionkey`, `v`, `s`, `email`, `joindate`, `last_ip`, `failed_logins`, `locked`, `last_login`, `active_realm_id`, `expansion`, `mutetime`, `locale`, `os`, `playerBot`) VALUES 
-(1,'ADMINISTRATOR','a34b29541b87b7e4823683ce6c7bf6ae68beaaac',3,'','0','0','','2006-04-25 13:18:56','127.0.0.1',0,0,'0000-00-00 00:00:00',0,0,0,0,'','\0'),
-(2,'GAMEMASTER','7841e21831d7c6bc0b57fbe7151eb82bd65ea1f9',2,'','0','0','','2006-04-25 13:18:56','127.0.0.1',0,0,'0000-00-00 00:00:00',0,0,0,0,'','\0'),
-(3,'MODERATOR','a7f5fbff0b4eec2d6b6e78e38e8312e64d700008',1,'','0','0','','2006-04-25 13:19:35','127.0.0.1',0,0,'0000-00-00 00:00:00',0,0,0,0,'','\0'),
-(4,'PLAYER','3ce8a96d17c5ae88a30681024e86279f1a38c041',0,'','0','0','','2006-04-25 13:19:35','127.0.0.1',0,0,'0000-00-00 00:00:00',0,0,0,0,'','\0');
+(1,'ADMINISTRATOR','a34b29541b87b7e4823683ce6c7bf6ae68beaaac',3,'','0','0','','2021-01-01 13:18:56','127.0.0.1',0,0,'2021-01-01 13:18:56',0,0,0,0,'','\0'),
+(2,'GAMEMASTER','7841e21831d7c6bc0b57fbe7151eb82bd65ea1f9',2,'','0','0','','2021-01-01 13:18:56','127.0.0.1',0,0,'2021-01-01 13:18:56',0,0,0,0,'','\0'),
+(3,'MODERATOR','a7f5fbff0b4eec2d6b6e78e38e8312e64d700008',1,'','0','0','','2021-01-01 13:19:35','127.0.0.1',0,0,'2021-01-01 13:19:35',0,0,0,0,'','\0'),
+(4,'PLAYER','3ce8a96d17c5ae88a30681024e86279f1a38c041',0,'','0','0','','2021-01-01 13:19:35','127.0.0.1',0,0,'2021-01-01 13:19:35',0,0,0,0,'','\0');
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 --
@@ -96,15 +96,15 @@ DROP TABLE IF EXISTS `account_banned`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account_banned` (
-  `id` int(11) unsigned NOT NULL COMMENT 'The account ID (See account.id).',
-  `bandate` bigint(40) NOT NULL DEFAULT 0 COMMENT 'The date when the account was banned, in Unix time.',
-  `unbandate` bigint(40) NOT NULL DEFAULT 0 COMMENT 'The date when the account will be automatically unbanned.',
-  `bannedby` varchar(50) NOT NULL COMMENT 'The character that banned the account.',
-  `banreason` varchar(255) NOT NULL COMMENT 'The reason for the ban.',
-  `active` tinyint(4) NOT NULL DEFAULT 1 COMMENT 'Is the ban is currently active or not.',
+  `id` INT(11) UNSIGNED NOT NULL COMMENT 'The account ID (See account.id).',
+  `bandate` BIGINT(40) NOT NULL DEFAULT 0 COMMENT 'The date when the account was banned, in Unix time.',
+  `unbandate` BIGINT(40) NOT NULL DEFAULT 0 COMMENT 'The date when the account will be automatically unbanned.',
+  `bannedby` VARCHAR(50) NOT NULL COMMENT 'The character that banned the account.',
+  `banreason` VARCHAR(255) NOT NULL COMMENT 'The reason for the ban.',
+  `active` TINYINT(4) NOT NULL DEFAULT 1 COMMENT 'Is the ban is currently active or not.',
   PRIMARY KEY (`id`,`bandate`),
   CONSTRAINT `account_banned_ibfk_1` FOREIGN KEY (`id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Ban List';
+) ENGINE=INNODB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Ban List';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,13 +125,13 @@ DROP TABLE IF EXISTS `ip_banned`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ip_banned` (
-  `ip` varchar(32) NOT NULL DEFAULT '0.0.0.0' COMMENT 'The IP address that is banned.',
-  `bandate` bigint(40) NOT NULL COMMENT 'The date when the IP was first banned, in Unix time.',
-  `unbandate` bigint(40) NOT NULL COMMENT 'The date when the IP will be unbanned in Unix time.',
-  `bannedby` varchar(50) NOT NULL DEFAULT '[Console]' COMMENT 'The name of the character that banned the IP.',
-  `banreason` varchar(255) NOT NULL DEFAULT 'no reason' COMMENT 'The reason given for the IP ban.',
+  `ip` VARCHAR(32) NOT NULL DEFAULT '0.0.0.0' COMMENT 'The IP address that is banned.',
+  `bandate` BIGINT(40) NOT NULL COMMENT 'The date when the IP was first banned, in Unix time.',
+  `unbandate` BIGINT(40) NOT NULL COMMENT 'The date when the IP will be unbanned in Unix time.',
+  `bannedby` VARCHAR(50) NOT NULL DEFAULT '[Console]' COMMENT 'The name of the character that banned the IP.',
+  `banreason` VARCHAR(255) NOT NULL DEFAULT 'no reason' COMMENT 'The reason given for the IP ban.',
   PRIMARY KEY (`ip`,`bandate`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Banned IPs';
+) ENGINE=INNODB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Banned IPs';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -151,13 +151,13 @@ DROP TABLE IF EXISTS `realmcharacters`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `realmcharacters` (
-  `realmid` int(11) unsigned NOT NULL COMMENT 'The ID of the realm (See realmlist.id).',
-  `acctid` int(11) unsigned NOT NULL COMMENT 'The account ID (See account.id).',
-  `numchars` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'The number of characters the account has on the realm.',
+  `realmid` INT(11) UNSIGNED NOT NULL COMMENT 'The ID of the realm (See realmlist.id).',
+  `acctid` INT(11) UNSIGNED NOT NULL COMMENT 'The account ID (See account.id).',
+  `numchars` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'The number of characters the account has on the realm.',
   PRIMARY KEY (`realmid`,`acctid`),
   KEY `acctid` (`acctid`),
   CONSTRAINT `realmcharacters_ibfk_1` FOREIGN KEY (`realmid`) REFERENCES `realmlist` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Realm Character Tracker';
+) ENGINE=INNODB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Realm Character Tracker';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -177,21 +177,21 @@ DROP TABLE IF EXISTS `realmlist`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `realmlist` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'The realm ID.',
-  `name` varchar(32) NOT NULL DEFAULT '' COMMENT 'The name of the realm.',
-  `address` varchar(32) NOT NULL DEFAULT '127.0.0.1' COMMENT 'The public IP address of the world server.',
-  `localAddress` varchar(255) NOT NULL DEFAULT '127.0.0.1' COMMENT 'The local IP address of the world server.',
-  `localSubnetMask` varchar(255) NOT NULL DEFAULT '255.255.255.0' COMMENT 'The subnet mask used for the local network. ',
-  `port` int(11) NOT NULL DEFAULT 8085 COMMENT 'The port that the world server is running on.',
-  `icon` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'The icon of the realm.',
-  `realmflags` tinyint(3) unsigned NOT NULL DEFAULT 2 COMMENT 'Supported masks for the realm.',
-  `timezone` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'The realm timezone.',
-  `allowedSecurityLevel` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'Minimum account (see account.gmlevel) required for accounts to log in.',
-  `population` float unsigned NOT NULL DEFAULT 0 COMMENT 'Show the current population.',
-  `realmbuilds` varchar(64) NOT NULL DEFAULT '' COMMENT 'The accepted client builds that the realm will accept.',
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'The realm ID.',
+  `name` VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'The name of the realm.',
+  `address` VARCHAR(32) NOT NULL DEFAULT '127.0.0.1' COMMENT 'The public IP address of the world server.',
+  `localAddress` VARCHAR(255) NOT NULL DEFAULT '127.0.0.1' COMMENT 'The local IP address of the world server.',
+  `localSubnetMask` VARCHAR(255) NOT NULL DEFAULT '255.255.255.0' COMMENT 'The subnet mask used for the local network. ',
+  `port` INT(11) NOT NULL DEFAULT 8085 COMMENT 'The port that the world server is running on.',
+  `icon` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'The icon of the realm.',
+  `realmflags` TINYINT(3) UNSIGNED NOT NULL DEFAULT 2 COMMENT 'Supported masks for the realm.',
+  `timezone` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'The realm timezone.',
+  `allowedSecurityLevel` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Minimum account (see account.gmlevel) required for accounts to log in.',
+  `population` FLOAT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Show the current population.',
+  `realmbuilds` VARCHAR(64) NOT NULL DEFAULT '' COMMENT 'The accepted client builds that the realm will accept.',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Realm System';
+) ENGINE=INNODB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Realm System';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -211,13 +211,13 @@ DROP TABLE IF EXISTS `uptime`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `uptime` (
-  `realmid` int(11) unsigned NOT NULL COMMENT 'The realm id (See realmlist.id).',
-  `starttime` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT 'The time when the server was started, in Unix time.',
-  `startstring` varchar(64) NOT NULL DEFAULT '' COMMENT 'The time when the server started, formated as a readable string.',
-  `uptime` bigint(20) unsigned NOT NULL DEFAULT 0 COMMENT 'The uptime of the server, in seconds.',
-  `maxplayers` smallint(5) unsigned NOT NULL DEFAULT 0 COMMENT 'The maximum number of players connected',
+  `realmid` INT(11) UNSIGNED NOT NULL COMMENT 'The realm id (See realmlist.id).',
+  `starttime` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'The time when the server was started, in Unix time.',
+  `startstring` VARCHAR(64) NOT NULL DEFAULT '' COMMENT 'The time when the server started, formated as a readable string.',
+  `uptime` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'The uptime of the server, in seconds.',
+  `maxplayers` SMALLINT(5) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'The maximum number of players connected',
   PRIMARY KEY (`realmid`,`starttime`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Uptime system';
+) ENGINE=INNODB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Uptime system';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -237,18 +237,18 @@ DROP TABLE IF EXISTS `warden_log`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `warden_log` (
-  `entry` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Log entry ID',
-  `check` smallint(5) unsigned NOT NULL COMMENT 'Failed Warden check ID',
-  `action` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'Action taken (enum WardenActions)',
-  `account` int(11) unsigned NOT NULL COMMENT 'The account ID of the player.',
-  `guid` int(11) unsigned NOT NULL DEFAULT 0 COMMENT 'Player GUID',
-  `map` int(11) unsigned DEFAULT NULL COMMENT 'The map id. (See map.dbc)',
-  `position_x` float DEFAULT NULL COMMENT 'The x location of the player.',
-  `position_y` float DEFAULT NULL COMMENT 'The y location of the player.',
-  `position_z` float DEFAULT NULL COMMENT 'The z location of the player.',
-  `date` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'The date/time when the log entry was raised, in Unix time.',
+  `entry` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Log entry ID',
+  `check` SMALLINT(5) UNSIGNED NOT NULL COMMENT 'Failed Warden check ID',
+  `action` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Action taken (enum WardenActions)',
+  `account` INT(11) UNSIGNED NOT NULL COMMENT 'The account ID of the player.',
+  `guid` INT(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Player GUID',
+  `map` INT(11) UNSIGNED DEFAULT NULL COMMENT 'The map id. (See map.dbc)',
+  `position_x` FLOAT DEFAULT NULL COMMENT 'The x location of the player.',
+  `position_y` FLOAT DEFAULT NULL COMMENT 'The y location of the player.',
+  `position_z` FLOAT DEFAULT NULL COMMENT 'The z location of the player.',
+  `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() COMMENT 'The date/time when the log entry was raised, in Unix time.',
   PRIMARY KEY (`entry`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Warden log of failed checks';
+) ENGINE=INNODB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Warden log of failed checks';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -256,8 +256,8 @@ CREATE TABLE `warden_log` (
 --
 
 LOCK TABLES `warden_log` WRITE;
-/*!40000 ALTER TABLE `uptime` DISABLE KEYS */;
-/*!40000 ALTER TABLE `uptime` ENABLE KEYS */;
+/*!40000 ALTER TABLE `warden_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `warden_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
